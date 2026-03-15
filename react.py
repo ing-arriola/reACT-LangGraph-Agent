787770cf9e879d5ff8f837d6a6e67414c132236f
ing-arriola <ingeniero.arriola@gmail.com>
2026-03-15 06:10:45 -0400

    started react agent

---
+++ react.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_groq import Groq
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """ 
+    param num: a number to be tripled
+    returns: the tripled number
+    """
+    return num *3
+
+
+tools = [triple,TavilySearch(max_results=1)]
+
+llm = Groq(model="llama3-8b-8192",api_key=os.getenv("GROQ_API_KEY")).bind_tools(tools)
